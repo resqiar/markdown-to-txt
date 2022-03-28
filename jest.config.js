a4bbe57ea505b626b8916c1bef4473ee4e8f47de
resqiar <resqiageng@gmail.com>
2022-03-28 20:01:11 +0700

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+	preset: "ts-jest",
+	testEnvironment: "node",
+	testRegex: ".*\\.spec\\.ts$",
+	transform: {
+		".+\\.(t|j)s$": "ts-jest",
+	},
 };
